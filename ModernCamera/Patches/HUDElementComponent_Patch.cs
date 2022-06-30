c6aabcb063f5a6b5e102de9fc49401179eea0c19
iZastic <izastic@gmail.com>
2022-06-30 04:00:49 -0600

    Missing files

---
+++ ModernCamera/Patches/HUDElementComponent_Patch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using ProjectM.UI;
+using UnityEngine;
+
+namespace ModernCamera.Patches;
+
+[HarmonyPatch]
+internal static class HUDElementComponent_Patch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(HUDElementComponent), nameof(HUDElementComponent.UpdateVisibility))]
+    private static void UpdateVisibility(HUDElementComponent __instance)
+    {
+        var canvasGroup = __instance.GetComponentInChildren<CanvasGroup>();
+        if (canvasGroup != null)
+            canvasGroup.alpha = 1f;
+    }
+}
